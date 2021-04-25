61979e6bcd632c5865a2083425a01c8242efb07a
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-04-25 20:31:43 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import reactDOM from "react-dom"; // latest es6 version of javascript
+
+reactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>I am a boy from another world</p>
+  </div>,
+  document.getElementById("root")
+);

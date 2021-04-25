0977008660c303cdfe69969e32e55ae8b36ea5c6
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-04-25 21:20:08 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import react from "react";
 import reactDOM from "react-dom"; // latest es6 version of javascript
 
+// if we want to insert the javascript inside the html then we have to simply write {variable name}
+const Fname = "Manan";
+const LName = "Aggarwal";
+// we can add any javasscript expression inside the {}.
+// we can use as many curly braces as we can
 reactDOM.render(
   <div>
-    <h1>List of the Stationary</h1>
-    <ul>
-      <li>Pencil</li>
-      <li>pen</li>
-      <li>Eraser</li>
-    </ul>
+    <h1>Hello {Fname + " " + LName}</h1>
   </div>,
   document.getElementById("root")
 );
+
+// By using String interpolation
+reactDOM.render(
+  <div>
+    <h1>Hello {`${Fname} ${LName}`}</h1>
+  </div>,
+  document.getElementById("root")
+);
+
+// reactDOM.render(
+//   <div>
+//     <h1>List of the Stationary</h1>
+//     <ul>
+//       <li>Pencil</li>
+//       <li>pen</li>
+//       <li>Eraser</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 // reactDOM.render(
 //   <div>

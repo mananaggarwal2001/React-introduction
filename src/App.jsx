941688df59236236bd16932d089ed92050f372bb
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-05-01 18:53:36 +0530

    react commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Heading from "./Heading";
 import List from "./List";
 
 function App(name) {
-  // react property like html we can also define the custom properties in the react app.
+  // react property like html we can also define the custom properties in the react app and we can also extract the details like javascript object.
   console.log(name);
   return (
     <div>

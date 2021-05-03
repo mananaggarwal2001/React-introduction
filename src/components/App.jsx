ec7adb0699d4e095b65b75990d9ddce9d4ec10f7
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-05-03 23:30:29 +0530

    reactForm Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
+/********************************REACT FORMS*******************************************/
 function App() {
     const [name, setheadingName] = useState();
     const [isMouseHover, setColor] = useState(false);
+    const [headingText, SetName] = useState("");
+    let value = "";
 
-    function handleClicking() {
-        setheadingName('GOODBYE');
+    function handleChange(event) {
+        setheadingName(event.target.value);
     }
 
     function mouseHover() {
@@ -15,16 +18,31 @@ function App() {
     function MouseOut() {
         setColor(false);
     }
+
+    function handleClick() {
+        SetName(name);
+    }
+
+    // function handleChange(event) { // event is the parameter which is used to observe the changes
+    //     console.log(event.target.value);
+    //     console.log(event.target.placeholder); // used for observing the changes in the value.
+    //     console.log(event.target.type);
+
+    //     setheadingName(event.target.value);
+    // we can also set the onSubmit property of the form to tell how to hasndle the Submit event of the button.
+    // event.preventDefault() prevent the default behaviour of the particular element such as form.
+    // }
     return ( <
         div className = "container" >
         <
-        h1 > { name } < /h1> <
-        input type = "text"
+        h1 > { headingText } < /h1> <
+        input onChange = { handleChange }
+        type = "text"
         placeholder = "What's your name?" / >
 
 
         <
-        button onClick = { handleClicking }
+        button onClick = { handleClick }
         style = {
             {
                 backgroundColor: isMouseHover ? "Black" : "White"
@@ -38,5 +56,9 @@ function App() {
 
     )
 }
+
+// The HTML elements are responsible for handling their own state in the html.
+// these elements are called controlled components like the value property in the input tag.
+
 
 export default App;

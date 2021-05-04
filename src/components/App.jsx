267b440a4784c25a8d4fe06cea00e13e39988b87
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-05-04 20:06:56 +0530

    ComplexState Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,33 +10,43 @@ function App() {
     function handleChange(event) {
         const eventValue = event.target.value;
         const eventName = event.target.name;
-        if (eventName === "fName") {
-            setContact((prevValue) => {
-                return {
-                    fName: eventValue,
-                    lName: prevValue.lName,
-                    email: prevValue.email
-                }
-            });
-        } else if (eventName === "lName") {
-            setContact((prevValue) => {
-                return {
-                    fName: prevValue.fName,
-                    lName: eventValue,
-                    email: prevValue.email
-                }
-            });
+        const { name, value } = event.target;
+        // if (eventName === "fName") {
+        //     setContact((prevValue) => {
+        //         return {
+        //             fName: eventValue,
+        //             lName: prevValue.lName,
+        //             email: prevValue.email
+        //         }
+        //     });
+        // } else if (eventName === "lName") {
+        //     setContact((prevValue) => {
+        //         return {
+        //             fName: prevValue.fName,
+        //             lName: eventValue,
+        //             email: prevValue.email
+        //         }
+        //     });
 
-        } else if (eventName === "email") {
-            setContact((prevValue) => {
-                return {
-                    fName: prevValue.fName,
-                    lName: prevValue.lName,
-                    email: eventValue
-                }
-            });
+        // } else if (eventName === "email") {
+        //     setContact((prevValue) => {
+        //         return {
+        //             fName: prevValue.fName,
+        //             lName: prevValue.lName,
+        //             email: eventValue
+        //         }
+        //     });
 
-        }
+        // } // insted of this we can write
+
+
+        setContact(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value // in the box if we take the name then it will replace the value and without boxes it will simply take the key name and replace it with the valueName
+
+            };
+        });
     }
 
     return ( <
